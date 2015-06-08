ee3704ec1ca7fe12e1095e4e0e936ec2d237c145
Evan <evantsaip@gmail.com>
2015-06-08 23:26:43 +0800

    First Commit

---
+++ Day#14HwAccupass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Day#14HwAccupass
+//
+//  Created by lalaleelala on 6/8/15.
+//  Copyright (c) 2015 lalaleelala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Day#14HwAccupass/ViewController.h
@@ -0,0 +1,24 @@
+//
+//  ViewController.h
+//  Day#14HwAccupass
+//
+//  Created by lalaleelala on 6/8/15.
+//  Copyright (c) 2015 lalaleelala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    IBOutlet UILabel *explo;
+    IBOutlet UILabel *search;
+    IBOutlet UILabel *cupon;
+    IBOutlet UILabel *live;
+    IBOutlet UILabel *setting;
+    IBOutlet UILabel *logout;
+    
+    
+}
+
+@end
+
